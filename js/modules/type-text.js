9deb2c03140ebb8bb3a7a64f8211c88ed9853265
artkir <effest@gmail.com>
2025-01-04 22:26:00 +0300

    feat: Add typing text

--- js/modules/type-text.js
+++ js/modules/type-text.js
@@ -1,42 +1,107 @@
 const heroCanDoTexts = [
-	'Запускаем IT проекты',
-	'Автоматизируем бизнес-процессы',
-	'Настраиваем нейросети',
-	'Верстаем сайты и приложения',
-	'Разрабатываем образовательные платформы',
+	'Мы&nbsp;помогаем запускать&nbsp;IT продукты',
+	'Мы&nbsp;автоматизируем бизнес-процессы.',
+	'Мы&nbsp;обучаем нейросети&nbsp;заказчиков.',
+	'Мы&nbsp;верстаем и&nbsp;запускаем сайты.',
+	'Мы&nbsp;разрабатываем образовательные платформы.',
 ];
 
-// Check all elements are loaded
-window.onload = () => {
-	console.log('Страница и все скрипты и загружены!');
+const TYPING_SPEED = 40;
+const ERASING_SPEED = 10;
+const PAUSE_BEFORE_ERASING = 4000;
+const PAUSE_BEFORE_TYPING = 100;
 
-	// Init typing on Hero section
-	initHeroTextRotation();
-};
+class TypingAnimation {
+	constructor(element, texts) {
+		this.element = element;
+		this.texts = texts;
+		this.currentTextIndex = 0;
+		this.currentText = '';
+		this.blinkInterval = null;
 
-const findHeroDescriptionElement = () => {
-	return document.querySelector('.hero__description p:nth-child(2)');
-};
+		this.textNode = document.createTextNode('\u200B');
+		this.cursorNode = document.createTextNode('|');
 
-const createTextRotationCycle = (element, textArray) => {
-	let currentIndex = 0;
+		this.element.textContent = '';
+		this.element.appendChild(this.textNode);
+		this.element.appendChild(this.cursorNode);
 
-	return () => {
-		element.textContent = textArray[currentIndex];
-		currentIndex = (currentIndex + 1) % textArray.length;
-	};
-};
+		this.startCursorBlink();
+	}
 
-const initHeroTextRotation = () => {
-	const heroDescriptionElement = findHeroDescriptionElement();
-	const rotateHeroText = createTextRotationCycle(
-		heroDescriptionElement,
-		heroCanDoTexts
+	getCurrentTargetText() {
+		const div = document.createElement('div');
+		div.innerHTML = this.texts[this.currentTextIndex];
+		return div.textContent;
+	}
+
+	startCursorBlink() {
+		if (this.blinkInterval) {
+			clearInterval(this.blinkInterval);
+		}
+		this.blinkInterval = setInterval(() => {
+			this.cursorNode.nodeValue = this.cursorNode.nodeValue === '|' ? '' : '|';
+		}, 500);
+	}
+
+	stopCursorBlink() {
+		if (this.blinkInterval) {
+			clearInterval(this.blinkInterval);
+			this.cursorNode.nodeValue = '|';
+		}
+	}
+
+	async eraseText() {
+		this.stopCursorBlink();
+		while (this.currentText.length > 0) {
+			this.currentText = this.currentText.slice(0, -1);
+			this.updateText();
+			await this.sleep(ERASING_SPEED);
+		}
+		this.startCursorBlink();
+	}
+
+	async typeText() {
+		this.stopCursorBlink();
+		const targetText = this.getCurrentTargetText();
+		while (this.currentText.length < targetText.length) {
+			this.currentText = targetText.slice(0, this.currentText.length + 1);
+			this.updateText();
+			await this.sleep(TYPING_SPEED);
+		}
+		this.startCursorBlink();
+	}
+
+	updateText() {
+		// Используем Zero Width Space (невидимый символ) вместо неразрывного пробела
+		this.textNode.nodeValue = this.currentText || '\u200B';
+	}
+
+	sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	async animate() {
+		while (true) {
+			await this.sleep(PAUSE_BEFORE_ERASING);
+			await this.eraseText();
+
+			this.currentTextIndex = (this.currentTextIndex + 1) % this.texts.length;
+
+			await this.sleep(PAUSE_BEFORE_TYPING);
+			await this.typeText();
+		}
+	}
+}
+
+function initTextAnimation() {
+	const textElement = document.querySelector(
+		'.hero__description p:nth-child(2)'
 	);
+	if (!textElement) return;
 
-	// Первоначальная установка текста
-	rotateHeroText();
+	const animation = new TypingAnimation(textElement, heroCanDoTexts);
+	animation.animate();
+}
 
-	// Запуск интервала для смены текста каждые 3 секунды
-	setInterval(rotateHeroText, 3000);
-};
+window.addEventListener('load', initTextAnimation);
