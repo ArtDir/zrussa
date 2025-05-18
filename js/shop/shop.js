1f221c668dea005316155390aa5c75ce64b948a3
artkir <effest@gmail.com>
2025-05-18 18:21:26 +0300

    feat: Add content

--- js/shop/shop.js
+++ js/shop/shop.js
@@ -1,18 +1,18 @@
 // Основной файл JS для страницы магазина
+import ThemeSwitch from '../components/theme-switch.js';
+import '../modules/mobile-menu.js';
+import MobileMenu from '../modules/mobile-menu.js';
 import '../modules/popup.js';
 import '../modules/scrolling.js';
-import '../modules/mobile-menu.js';
-import './shop-cards.js';
 import './shop-add-button.js';
-import ThemeSwitch from '../components/theme-switch.js';
-import MobileMenu from '../modules/mobile-menu.js';
+import './shop-cards.js';
 
 // Инициализация компонентов
 document.addEventListener('DOMContentLoaded', () => {
-  // Инициализируем общие компоненты
-  new ThemeSwitch();
-  new MobileMenu();
-  
-  // Инициализация специфичных компонентов магазина
-  console.log('Страница магазина инициализирована');
+	// Инициализируем общие компоненты
+	new ThemeSwitch();
+	new MobileMenu();
+
+	// Инициализация специфичных компонентов магазина
+	console.log('Страница магазина инициализирована');
 });
